db15d92274f7830fa2e609ada33de3fe0e783e5a
velo24dl <velo24dl@student.ju.se>
2025-02-14 14:28:00 +0100

    Added products on products page, added filter button, added individual HTML files for all product

---
+++ javascript/main.js
@@ -0,0 +1,7 @@
+const nextButton = document.querySelector("next-img");
+const productImg = document.getElementById("next-img");
+
+// nextButton.addEventListener("click", function(event) {
+//     productImg.innerHTML =
+
+// })
