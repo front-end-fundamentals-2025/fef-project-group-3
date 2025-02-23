5134342e50c5e4e6e2ad3a6a106fbdb159693ebd
velo24dl <velo24dl@student.ju.se>
2025-02-23 17:34:43 +0100

    Tried to fix buttons on product 1 page

--- javascript/main.js
+++ javascript/main.js
@@ -1,4 +1,19 @@
-const nextButtonElement = document.getElementsByClassName("next-img");
-const imagesSlidesElement = document.getElementsByClassName(
-  "product1-img-container"
-);
+// const nextButtonElement = document.getElementsByClassName("next-img");
+// const imagesSlidesElement = document.getElementsByClassName(
+//   "product1-img-container"
+// );
+
+/*Specifications buttons - Start*/
+
+const contentsButton = document.querySelector("product-contents");
+// const specificationsButton = document.getElementsByClassName(
+//   "product-specifications"
+// );
+// const reviewsButton = document.getElementsByClassName("product-reviews");
+const contentsInfo = document.getElementsByClassName("product-contents-info");
+
+contentsButton.addEventListener("click", function (event) {
+  contentsInfo.innerText = "";
+});
+
+/*Specifications buttons - End*/
