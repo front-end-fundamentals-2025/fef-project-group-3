620c7e308770b54d5776e5767ccde746dbc02f3e
velo24dl <velo24dl@student.ju.se>
2025-02-26 14:45:53 +0100

    A little bit everywhere

--- javascript/main.js
+++ javascript/main.js
@@ -1,28 +1,49 @@
-// const nextButtonElement = document.getElementsByClassName("next-img");
-// const imagesSlidesElement = document.getElementsByClassName(
-//   "product1-img-container"
-// );
+/*Kit Contents buttons - Start*/
 
-/*Specifications buttons - Start*/
-
-const contentsButton = document.querySelector("product-contents");
-const contentsDropDown = document.getElementsByClassName(
-  "dropdown-product-contents"
-);
-// const specificationsButton = document.getElementsByClassName(
-//   "product-specifications"
-// );
-// const reviewsButton = document.getElementsByClassName("product-reviews");
+const contentsButton = document.querySelector(".product-contents");
+const contentsDropDown = document.querySelector(".dropdown-product-contents");
 
 contentsButton.addEventListener("click", function (event) {
-  contentsDropDown.innerText =
-    "repudiandae officia quod voluptas? Voluptatibus quasi repudiandae eligendi pariatur voluptatem, solut cupiditate nam molestias quod ducimus eum consequatur. Eos ration mollitia consequuntur doloremque a sit! Aspernatur repellendus reru corporis ut consequuntur numquam voluptas laborum! Consequuntur voluptas cum suscipit eligendi deserunt corrup";
+  contentsDropDown.classList.toggle("dropdown-product-contents-show");
 });
 
-/*Specifications buttons - End*/
+/*kit contents buttons - End*/
+
+/* More information button - Start*/
+
+const informationButton = document.querySelector(".product-specifications");
+const informationDropDown = document.querySelector(
+  ".dropdown-product-specifications"
+);
+
+informationButton.addEventListener("click", function (event) {
+  informationDropDown.classList.toggle("dropdown-product-specifications-show");
+});
+
+/* More information button - End*/
+
+/* Reviews button - Start*/
+
+const reviewsButton = document.querySelector(".product-reviews");
+const reviewsDropDown = document.querySelector(".dropdown-product-reviews");
+
+reviewsButton.addEventListener("click", function (event) {
+  reviewsDropDown.classList.toggle("dropdown-product-reviews-show");
+});
+
+/* Reviews Button - End*/
+
+/* Switching Pictures Product 1 start help https://www.w3schools.com/css/css_z-index.asp*/
+
+const nextButton = document.querySelector(".next-img");
+const productImages = document.querySelector(".product-images");
+
+nextButton.addEventListener("click", function (event) {
+  productImages.style.backgroundColour = "000000";
+});
 
 /* SHOPPING CART TAB */
-let iconCart = document.querySelector('.icon-cart');
+let iconCart = document.querySelector(".icon-cart");
 let closeCart = document.querySelector(".close");
 let body = document.querySelector("body");
 
