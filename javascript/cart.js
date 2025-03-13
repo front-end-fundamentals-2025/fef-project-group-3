142af75d78e1306c3dc8adf1b05af2ead4f849fd
velo24dl <velo24dl@student.ju.se>
2025-03-13 11:46:59 +0100

    cart

--- javascript/cart.js
+++ javascript/cart.js
@@ -1,7 +1,11 @@
-/* SHOPPING CART TAB */
+//youtube video//
 const iconCart = document.querySelector("#cart_icon");
 const closeCart = document.querySelector(".close_carttab");
 const cartTab = document.querySelector(".cartTab");
+const add_to_cartButton = document.querySelectorAll(".add-to-cart");
+let listCartHTML = document.querySelector(".listCart");
+
+let listCart = [];
 
 iconCart.addEventListener("click", function (event) {
   cartTab.classList.toggle("showCart");
@@ -11,70 +15,28 @@ closeCart.addEventListener("click", function (event) {
   cartTab.classList.toggle("showCart");
 });
 
-/* CORRECT METHOD W/ OBJECT (not done at all) */
-
-// Store object
-
-/* localStorage.student = JSON.stringify(student);
-
-// Fetch object
-let newStudent = JSON.parse(localStorage.student);
-console.log(newStudent.name);
- */
-
-/* ADD ITEM TO CART */
-const cart_itemElement = document.getElementsByClassName("cart_item");
-const add_to_cartElement = document.querySelectorAll(".add-to-cart");
-
-/* if (localStorage.item) {
-    cart_itemElement. = localStorage. ;
-} else {
-    cart_itemElement. = "";
-}
- */
-
-/*Spara i local Storage sen if statement*/
-// add_to_cartElement.addEventListener("click", function (event) {
-//   let cart_item = innerList.value;
-//   innerList.innerHTML = cart_item;
-//   localStorage.cart_item = cart_item;
+// add_to_cartButton.forEach((add_to_cartButton) => {
+//   const ourList = document.getElementById("inner-list");
 // });
 
-/* done with the help of lab assistents */
-add_to_cartElement.forEach((add_to_cartElement) => {
-  const ourList = document.getElementById("inner-list");
-
+const initApp = () => {
   fetch("javascript/products.json")
     .then((response) => response.json())
     .then((products) => {
-      add_to_cartElement.addEventListener("click", function (event) {
-        cartTab.classList.toggle("showCart");
-
-        const dataId = add_to_cartElement.getAttribute("data-id");
-        const addItem = products[dataId];
-        const itemDetails =
-          "<li class='cart-item'><img src='" +
-          addItem.image +
-          "' alt='" +
-          addItem.name +
-          "' class='cart-item-image'> <span class='cart-item-name'> '" +
-          addItem.productName +
-          "'</span> <span class='cart-item-price'> '" +
-          addItem.productPrice +
-          "'</span> '";
-        "<div class='cart-item-quantity'>" +
-          "'<span class='quantity_minus'>-</span>'" +
-          "'<span class='quantity_number'>1</span>'" +
-          "'<span class='quantity_plus'>+</span>'" +
-          "</div>" +
-          "</li>";
-
-        ourList.innerHTML += itemDetails;
-        console.log(addItem.productPrice);
-
-        localStorage.itemDetails =
-          JSON.stringify.itemDetails(add_to_cartElement);
-        let newitemDetails = JSON.parse(localStorage.itemDetails);
-      });
+      listCart = products;
+      console.log(listCart);
     });
-});
+};
+initApp();
+
+// add_to_cartButton.forEach((add_to_cartButton) => {
+//     const product = document.getElementById("inner-list");
+//   });
+
+//   add_to_cartButton.addEventListener("click", function (event) {
+//     cartTab.classList.toggle("showCart");
+//   });
+
+// add_to_cartButton.addEventListener("click", function (event) {
+//     cartTab.classList.toggle("showCart");
+//   });
