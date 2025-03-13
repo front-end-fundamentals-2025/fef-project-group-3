264067cbea8cc47c7325c08b22cfc1c22b3be689
velo24dl <velo24dl@student.ju.se>
2025-03-13 16:34:31 +0100

    Cart

--- javascript/cart.js
+++ javascript/cart.js
@@ -3,7 +3,23 @@ const iconCart = document.querySelector("#cart_icon");
 const closeCart = document.querySelector(".close_carttab");
 const cartTab = document.querySelector(".cartTab");
 const innerList = document.querySelector("#inner-list");
-const cartItem = document.querySelector(".cart_item");
+const cartItem = document.createElement("li");
+const addToCartButton = document.querySelector(".add-to-cart");
+const removeItemButton = document.querySelector("#remove-item");
+
+cartItem.classList.add("cart_item");
+cartItem.innerText = "PARTY PRINCESS KIT - 149 KR";
+let item = "PARTY PRINCESS KIT - 149KR";
+
+addToCartButton.addEventListener("click", function (event) {
+  innerList.appendChild(cartItem);
+  let newItem = cartItem.value;
+  localStorage.newItem = newItem;
+});
+
+removeItemButton.addEventListener("click", function (event) {
+  cartItem.remove();
+});
 
 iconCart.addEventListener("click", function (event) {
   cartTab.classList.toggle("showCart");
@@ -11,8 +27,4 @@ iconCart.addEventListener("click", function (event) {
 
 closeCart.addEventListener("click", function (event) {
   cartTab.classList.toggle("showCart");
-});
-
-iconCart.addEventListener("click", function (event) {
-  innerList.appendChild(cartItem);
 });
